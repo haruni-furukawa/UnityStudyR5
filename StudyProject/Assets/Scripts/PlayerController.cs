be851738e9da062e7d07b42fb853f58ecc0a9b8a
haruni-furukawa <furukawa@haru-ni.net>
2023-07-16 00:04:04 +0900

    [add] Playerのプレハブとスクリプトを追加

---
+++ StudyProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private int moveSpeed;
+    [SerializeField] private int jumpForce;
+    private bool isMoving = false;
+    private bool isJumping = false;
+    private bool isFalling = false;
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        isMoving = horizontal != 0;
+        isFalling = rb.velocity.y < -0.5f;
+
+        if (isMoving)
+        {
+            Vector3 scale = gameObject.transform.localScale;
+            if (horizontal < 0 && scale.x > 0 || horizontal > 0 && scale.x < 0)
+            {
+                scale.x *= -1;
+            }
+            gameObject.transform.localScale = scale;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && !isFalling)
+        {
+            Jump();
+        }
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        animator.SetBool("IsMoving", isMoving);
+        animator.SetBool("IsJumping", isJumping);
+        animator.SetBool("IsFalling", isFalling);
+    }
+    void Jump()
+    {
+        isJumping = true;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Stage"))
+        {
+            isJumping = false;
+        }
+    }
+}
